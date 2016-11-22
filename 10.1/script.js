1eb9e9c55e8b644008c5c0dd209f9f7fc849b87a
Zeynep <eflamisa@Zeyneps-MacBook-Pro.local>
2016-11-21 19:13:36 -0500

    zynp's week 10th asgmnt

--- 10.1/script.js
+++ 10.1/script.js
@@ -14,11 +14,27 @@ var plot = d3.select('.canvas')
 //Mapping specific functions
 //Projection
 
+var projection = d3.geoOrthographic()
+
 //Geopath
+
+var pathGenerator=d3.geoPath()
+    .projection(projection);
 
 d3.json('../data/world-50m.json',dataloaded);
 
 function dataloaded(err, data){
     console.log(data); //This is a Topojson data
     console.log(topojson.feature(data,data.objects.countries)); //This is the converted GeoJSON data of countries
+
+
+var newdata =topojson.feature(data, data.objects.countries);
+    console.log(newdata);
+    projection.fitExtent([[0,0],[w,h]],newdata);
+    var states=plot.selectAll('.state')
+        .data(newdata.features)
+        .enter()
+        .append('path').attr('class', 'state')
+        .attr('d', pathGenerator)
+
 }
